9fd278ea1f61a71d54059fcbd72a47c4c13663e7
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2023-02-21 03:07:56 +0900

    TypeScript化

---
+++ src/components/ImageList.tsx
@@ -0,0 +1,41 @@
+import './ImageList.css'
+import React, { FC } from 'react'
+import ImageCard from './ImageCard'
+import { ImageCardProps } from './ImageCard'
+
+type Props = {
+  images: ImageCardProps[]
+  term: string
+  page: number
+  setPage: (page: number) => void
+  onSearchSubmit: (term: string, page: number) => void
+}
+
+const ImageList: FC<Props> = (props: Props) => {
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />
+  })
+
+  const handleScroll = (e: any) => {
+    const element = e.target
+    const { scrollHeight, scrollTop, clientHeight } = element
+    if (scrollHeight - scrollTop === clientHeight) {
+      console.log('reach end')
+      const newPage = props.page + 1
+      props.setPage(newPage)
+      props.onSearchSubmit(props.term, newPage)
+    }
+  }
+
+  return (
+    <div
+      onScroll={handleScroll}
+      style={{ height: '1000px', overflow: 'scroll' }}
+      className="image-list"
+    >
+      {images}
+    </div>
+  )
+}
+
+export default ImageList
